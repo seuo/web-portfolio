72bf7364e16ee91069c91c8dced7ee2178e37b2f
Sam Robertson <samrob.nz@gmail.com>
2019-06-06 18:12:48 +1200

    web update scroll

--- myJavascript.js
+++ myJavascript.js
@@ -1,15 +1,46 @@
 $(function () {
 
-    // sectionHeader animation 
 
+    // sectionHeader animation 
+$(function(){
+    var hHover = anime({
+        targets: ['#portfolio'],
+        duration: 1,
+        scale:1.02,
+        autoplay: false,
+        easing: 'easeInOutQuad',
+        loop: false,
+      });
+
+      function over(){
+        hHover.play({
+          scale: 1,
+        });
+      }
+      function out(){
+        hHover.play({
+          scale: .98,
+        });
+      }
+ 
+
+      $('#portfolio').hover(over,out);
+
+    });
 
     var tl1 = anime.timeline({
 
-        easing:'easeInOutQuart',
+        easing:'linear',
         autoplay:false,
-        duration:3000,
+        duration:1000,
  
     });
+
+    tl1.add ({
+        targets:'.ul',
+
+
+    },.1);
 
     tl1.add ({
         targets:'#portfolio',
@@ -18,24 +49,23 @@ $(function () {
         rotateY:[0,40],  
         rotateZ:[0,-5],
         rotateX:[0,55],
-        opacity:[1,0],  
-    });
+
+    },0);
+    
     tl1.add ({
         targets:'#contact',
-        translateY:[0,'90vh'],
+        translateY:[0,'100vh'],
         translateX:[0,'-15vw'],
         rotate:[0,-30],
-        opacity:[1,0],
-        
-    });
+
+    },0);
     tl1.add ({
         targets:'#cv',
-        translateY:[0,'70vh'],
+        translateY:[0,'100vh'],
         translateX:[0,'-25vw'],
         rotate:[0,-15],
-        opacity:[1,0],
-        
-    });
+
+    },0);
 
 
 
@@ -45,11 +75,12 @@ $(function () {
 
             $(document).on('scroll',function(){
             var scrollTop = $(document).scrollTop();
-            var progress = (scrollTop-s2anime+0);
+            var progress = scrollTop;
             tl1.seek(tl1.duration * progress);
             console.log(tl1.duration);
             
         });
+        
     });
 
 
@@ -62,15 +93,28 @@ $(function () {
   
         if (e.deltaY > 0) { ////wheel down
             console.log(scrollTop);
+
                 if(scrollTop<section2Top){
-                    $('body,html').animate({scrollTop:section2Top});
+                    $('body,html').animate({
+                        scrollTop:section2Top,
+                }
+                , 750, 'linear', function() {
+                    // Animation complete.
+                  });
             }
         }
   
         if (e.deltaY < 0) { //wheel up
             console.log(scrollTop);
                 if(scrollTop<section2Top){
-                    $('body,html').animate({scrollTop:0});
+                    $('body,html').animate({
+                        scrollTop:0,
+                    });
+
             }
         }
+
+
+        
     });
+
