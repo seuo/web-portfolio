6da6fbdd68a2ccd1e00c021b0a4a6327904fae06
Sam Robertson <samrob.nz@gmail.com>
2019-06-14 01:51:12 +1200

    late night work

--- myJavascript.js
+++ myJavascript.js
@@ -1,5 +1,37 @@
 $(function () {
 
+    var section2Top = $('.s2').offset().top;
+
+    window.addEventListener('wheel', function(e) {
+            var scrollTop = $(document).scrollTop();
+  
+        if (e.deltaY > 0) { ////wheel down
+            console.log(scrollTop);
+
+                if(scrollTop<section2Top){
+                    $('body,html').animate({
+                        scrollTop:section2Top,
+                }
+                ,0, 'linear', function() {
+                    // Animation complete.
+                  });
+            }
+        }
+  
+        if (e.deltaY < 0) { //wheel up
+            console.log(scrollTop);
+                if(scrollTop<section2Top){
+                    $('body,html').animate({
+                        scrollTop:0,
+                    }
+                    ,0, 'linear', function() {
+                        // Animation complete.
+                      });
+            }
+        }
+
+    });
+
     var animeElement = anime.timeline({
         easing:'linear',
         autoplay:true,
@@ -11,7 +43,7 @@ $(function () {
         targets: '.element1 img:hover',
         translateY: [10,0],
         easing: 'easeInOutSine'
-      });
+    });
       
 
     var tl1 = anime.timeline({
@@ -19,9 +51,16 @@ $(function () {
         easing:'linear',
         autoplay:false,
         duration:1000,
-        
- 
     });
+    tl1.add ({
+        targets:'.hero',
+        backgroundColor:['#fff','rgb(251, 249, 237)'], 
+    },0);
+
+    tl1.add ({
+        targets:'.s2',
+        backgroundColor:['#fff','rgb(251, 249, 237)'], 
+    },0);
 
 
     tl1.add ({
@@ -51,48 +90,89 @@ $(function () {
         opacity:[1,0],
     },0);
 
+
     var screenHeight = $(window).height();
     var s1anime = $('.headerSection').offset().top;
 
-            $(document).on('scroll',function(){
-            var scrollTop = $(document).scrollTop();
-            var progress = scrollTop/screenHeight;
-            tl1.seek(tl1.duration * progress);
-            console.log(progress);
+        $(document).on('scroll',function(){
+        var scrollTop = $(document).scrollTop();
+        var progress = scrollTop/screenHeight;
+        tl1.seek(tl1.duration * progress);
+        console.log(progress);
             
         });
+
+
         var tl2 = anime.timeline({
             easing:'linear',
             autoplay:false,
             duration:1000,
             direction: 'alternate',
+            loop: true,
+            loopBegin: function(anim) {
+                loopBegan++;
+                beginLogEl.value = 'loop began : ' + loopBegan;
+              },
+              loopComplete: function(anim) {
+                loopCompleted++;
+                completeLogEl.value = 'loop completed : ' + loopCompleted;
+              }
         });
-    
+        
+
+        // tl2.add ({
+        //     targets:'.s2',
+        //     backgroundColor:['#fff','rgb(251, 249, 237)'], 
+        // },0);
+
         tl2.add ({
-            targets:'.s2',
-            backgroundColor:'#111', 
+            targets:'.element1',
+            top:['-25vh','-30vh'],
         },0);
 
         tl2.add ({
-            targets:'.element1 h1',
-            bottom:['-5em','2em'],
-            duration:2000,
+            targets:'.element1 h2',
+            translateY:[0,'3vh'],
+        },0);
+
+
+        tl2.add ({
+            targets:'.info',
+            color:'#333',
+        },0);
+
+        tl2.add ({
+            targets:'.platform1',
+            marginTop:['0vh','-1vh']
+        },0);
+
+        tl2.add ({
+            targets:'.logoWork',
+            translateY:['7vh','-3vh']
+            
         },0);
 
         tl2.add ({
             targets:'.info',
-            color:'#cdcdcd',
+            translateY:['-10vh','-12vh']
         },0);
+
+
 
         var screenHeight = $(window).height();
 
-        var s2anime = $('.s2').offset().top;
+        var s2anime = $('.section').offset().top;
 
-            $(document).on('scroll',function(){
+        $(document).on('scroll',function(){
             var scrollTop = $(document).scrollTop();
-            var progress = scrollTop/screenHeight;
-            tl2.seek(tl1.duration * progress);
+    
+            var progress = ((scrollTop-s2anime+100)/500);
+    
+            tl2.seek(tl2.duration * progress);
         });
+
+        
+        
     });
 
 
@@ -100,35 +180,5 @@ $(function () {
 
 
 
-    // var section2Top = $('.s2').offset().top;
 
-    // window.addEventListener('wheel', function(e) {
-    //         var scrollTop = $(document).scrollTop();
-  
-    //     if (e.deltaY > 0) { ////wheel down
-    //         console.log(scrollTop);
-
-    //             if(scrollTop<section2Top){
-    //                 $('body,html').animate({
-    //                     scrollTop:section2Top,
-    //             }
-    //             , 750, 'linear', function() {
-    //                 // Animation complete.
-    //               });
-    //         }
-    //     }
-  
-    //     if (e.deltaY < 0) { //wheel up
-    //         console.log(scrollTop);
-    //             if(scrollTop<section2Top){
-    //                 $('body,html').animate({
-    //                     scrollTop:0,
-    //                 });
-
-    //         }
-    //     }
-
-
-        
-    // });
 
